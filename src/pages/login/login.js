67d46a795c1aa3ca6ec3bc36dba05d950ceba0ff
Hertz-7 <mohammadaosaf33@gmail.com>
2023-08-01 12:55:54 +0500

    Shifted backend api to frontend.

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,7 @@
 
 import './login.css';
 import { useState ,useEffect} from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { useNavigate} from 'react-router-dom';
 function Login(props) {
    const [username, setUsername] = useState('');
@@ -12,22 +12,31 @@ function Login(props) {
         if (localStorage.getItem('token')!==null) 
           navigate('/home');
     
-      },[]);
-      const handleLogin = async () => {
-        try {
-          // make a post request to the login route with the username and password as data
-          const response = await axios.post('http://localhost:9002/loginapi', { username, password });
+      },);
+      // const handleLogin = async () => {
+      //   try {
+      //     // make a post request to the login route with the username and password as data
+      //     const response = await axios.post('http://localhost:9002/loginapi', { username, password });
       
-          // get the token from the response
-          const token = response.data;
+      //     // get the token from the response
+      //     const token = response.data;
+      //     localStorage.setItem('token', token);
+      //     navigate('/home');
+      
+      //   } catch (error) {
+      //     //if response status is 401 turn loginerr to true
+      //     if (error.response.status === 401){
+      //       setLoginerr(true);
+      //     }
+      //   }
+      // };
+      const handleLogin =  () => {
+        if (username === 'admin' && password === 'admin') {
+          const token = 'admin123';
           localStorage.setItem('token', token);
           navigate('/home');
-      
-        } catch (error) {
-          //if response status is 401 turn loginerr to true
-          if (error.response.status === 401){
-            setLoginerr(true);
-          }
+        }else{
+          setLoginerr(true);
         }
       };
       
